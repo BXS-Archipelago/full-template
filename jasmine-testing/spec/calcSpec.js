c97cabd0065e0759d6e3d3d1b6f0a2c4a8884be3
BXS-Archipelago <bxs@tutanota.com>
2021-02-10 16:37:12 +0000

    Jasmine Spies

--- jasmine-testing/spec/calcSpec.js
+++ jasmine-testing/spec/calcSpec.js
@@ -1,10 +1,19 @@
-describe("Calculator", function(){
-    describe("addition function", function(){
-        it("should return 42", function(){
-            expect(addition(20,22)).toBe(42);
-        })
+
+
+describe("Calculator", function() {
+    describe("Addition function", function() {
+        it("should return 42", function() {
+            expect(addition(20, 22)).toBe(42);
+        });
+
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26)
-        })
-    })
-})
+            expect(addition(7, 19)).toBe(26);
+        });
+
+        it("should return an error if we don't supply two numbers", function() {
+           spyOn(window,"alert");
+           addition("Hitchhikers", "Guide");
+           expect(window.alert).toHaveBeenCalledWith ("Error!");
+        });
+    });
+});
